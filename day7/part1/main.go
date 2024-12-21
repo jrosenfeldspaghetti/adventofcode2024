975837c0b837c3deb2ba51df600696cbda92d9aa
jrosenfeld <josh.rosenfeld@booking.com>
2024-12-21 23:24:25 +0100

    day 7

---
+++ day7/part1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	multiply = "multiply"
+	add      = "add"
+)
+
+func main() {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputStr := string(input)
+	total := 0
+	for _, calculation := range strings.Split(inputStr, "\n") {
+		splitOnColon := strings.Split(calculation, ":")
+		goal, _ := strconv.Atoi(splitOnColon[0])
+		numbersToCalc := []int{}
+		for _, num := range strings.Split(strings.TrimSpace(splitOnColon[1]), " ") {
+			numAsInt, _ := strconv.Atoi(num)
+			numbersToCalc = append(numbersToCalc, numAsInt)
+		}
+		if calculateOption(0, goal, numbersToCalc, add) || calculateOption(0, goal, numbersToCalc, multiply) {
+			total += goal
+		}
+
+	}
+	fmt.Println(total)
+}
+
+func calculateOption(current, goal int, vals []int, operator string) bool {
+	if current == goal && len(vals) == 0 {
+		return true
+	}
+	// short circuit some calculations
+	if current > goal {
+		return false
+	}
+
+	if operator == multiply {
+		newCurrent := current * vals[0]
+		if len(vals) == 1 {
+			return newCurrent == goal
+		}
+		if calculateOption(newCurrent, goal, vals[1:], multiply) || calculateOption(newCurrent, goal, vals[1:], add) {
+			return true
+		}
+	}
+	if operator == add {
+		newCurrent := current + vals[0]
+		if len(vals) == 1 {
+			return newCurrent == goal
+		}
+		if calculateOption(newCurrent, goal, vals[1:], multiply) || calculateOption(newCurrent, goal, vals[1:], add) {
+			return true
+		}
+	}
+	return false
+}
